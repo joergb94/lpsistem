64ed851aa1b4778dca65fc151303df3059471ad9
josergb01 <67196470+josergb01@users.noreply.github.com>
2020-09-03 13:52:24 -0500

    changes validate

---
+++ app/Helpers/UserAccessUrl.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Data_menu;
+use App\Type_user_detail;
+
+if (!function_exists('accesUrl')) {
+ 
+   
+    function accesUrl($user,$menu_id)
+    {
+   
+        //validate access of user     
+        $access=Type_user_detail::where('type_user_id',$user->type_user_id)
+                                ->where('data_menu_id',$menu_id)
+                                ->where('active',1)
+                                ->exists();
+        if($access == true){
+          //get all data for user menu
+          $menuU = Type_user_detail::where('type_user_id',  $user->type_user_id);
+              
+          $menuU->with(['data_menu' => function($query) {
+              $query->orderby('prioridad');
+            }]);
+
+          $data_menu=$menuU->get();
+          $type_user = $user->type_user_id;
+        
+        }else{
+          
+           $data_menu=[];
+           $type_user =0;
+        }
+       
+
+        $menu=[
+          'data_menu'=>$data_menu,
+          'access'=>$access,
+          'type_user'=>$type_user,
+        ];  
+   
+      return $menu;
+    }
+}
---
+++ app/Helpers/ValidateAccess.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Data_menu;
+use App\Type_user_detail;
+
+if (!function_exists('validateAccess')) {
+ 
+   
+    function validateAccess($user,$menu_id)
+    {
+        if($user && $menu_id){
+            //validate access of user     
+            $access=Type_user_detail::where('type_user_id',$user->type_user_id)
+                                    ->where('data_menu_id',$menu_id)
+                                    ->where('active',1)
+                                    ->exists();
+        }else{
+
+            $access=false;
+        }
+
+      return $access;
+    }
+}
